fa59d47fdf18fdd91185f76d80efbfb644aff890
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-20 00:59:52 -0300

    fix: should create address from pub key

--- src/account/wallet.rs
+++ src/account/wallet.rs
@@ -3,10 +3,16 @@ pub struct Wallet {
     public_key: Vec<u8>,
 }
 
+fn public_key_to_address(public_key: &Vec<u8>) -> Vec<u8> {
+    let mut address = vec![0u8; 20];
+    address.copy_from_slice(&public_key[0..20]);
+    address
+}
+
 impl Wallet {
-    pub fn new(address: Vec<u8>, public_key: Vec<u8>) -> Self {
+    pub fn new(public_key: Vec<u8>) -> Self {
         Self {
-            address,
+            address: public_key_to_address(&public_key),
             public_key,
         }
     }
