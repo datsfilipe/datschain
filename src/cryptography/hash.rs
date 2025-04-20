f2c43e4c12220a2d4c720707b532f5e2d27aa5cd
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-19 23:23:42 -0300

    fix: use references, embrace the borrow checker

--- src/cryptography/hash.rs
+++ src/cryptography/hash.rs
@@ -1,7 +1,7 @@
 use keccak_asm::Digest;
 use keccak_asm::Keccak256;
 
-pub fn transform(input: String) -> String {
+pub fn transform(input: &String) -> String {
     let mut keccak = Keccak256::new();
     keccak.update(input.as_bytes());
     let result = keccak.finalize();
@@ -12,7 +12,7 @@ pub fn transform(input: String) -> String {
     hex_hash
 }
 
-pub fn verify(input: String, hash: String) -> bool {
+pub fn verify(input: &String, hash: String) -> bool {
     let mut keccak = Keccak256::new();
     keccak.update(input.as_bytes());
     let result = keccak.finalize();
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use cryptography::hash::{transform, verify};
 
 fn main() {
     let message = String::from("hello world");
-    let data = transform(message.clone());
+    let data = transform(&message);
     println!("{:?}", "0x".to_owned() + &data);
 
-    let verify = verify(message, data);
+    let verify = verify(&message, data);
     println!("{:?}", verify);
 }
