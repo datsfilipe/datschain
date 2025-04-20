d51ecc2286901f85a5696b693f149cc3a2746cfa
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-20 01:54:50 -0300

    eat: move some things to conversion; create timestamp util

--- src/cryptography/hash.rs
+++ src/cryptography/hash.rs
@@ -1,15 +1,13 @@
 use keccak_asm::Digest;
 use keccak_asm::Keccak256;
 
+use crate::utils::conversion::to_string;
+
 pub fn transform(input: &String) -> String {
     let mut keccak = Keccak256::new();
     keccak.update(input.as_bytes());
     let result = keccak.finalize();
-    let hex_hash = result
-        .iter()
-        .map(|b| format!("{:02x}", b))
-        .collect::<String>();
-    hex_hash
+    to_string(&result)
 }
 
 pub fn verify(input: &String, hash: String) -> bool {
--- src/utils/conversion.rs
+++ src/utils/conversion.rs
@@ -7,3 +7,10 @@ pub fn to_hex(bytes: &[u8]) -> String {
     }
     s
 }
+
+pub fn to_string(bytes: &[u8]) -> String {
+    bytes
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect::<String>()
+}
