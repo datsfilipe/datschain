80ade8b0ba398bf697603396ebda1c1392af8bb0
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-20 00:21:34 -0300

    feat: add signature example in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod cryptography;
+mod utils;
 
 use cryptography::hash::{transform, verify};
+use cryptography::signature;
+use utils::conversion::to_hex;
 
 fn main() {
     let message = String::from("hello world");
@@ -8,5 +11,20 @@ fn main() {
     println!("{:?}", "0x".to_owned() + &data);
 
     let verify = verify(&message, data);
+    println!("{:?}", verify);
+
+    let (secret_key, public_key) = signature::generate_keypair();
+    println!("{:?}", to_hex(&secret_key));
+    println!("{:?}", to_hex(&public_key));
+
+    let bytes = message.into_bytes();
+    let signature = signature::sign(&bytes, &secret_key.try_into().unwrap());
+    println!("{:?}", to_hex(&signature));
+
+    let verify = signature::verify(
+        &bytes,
+        &signature.try_into().unwrap(),
+        &public_key.try_into().unwrap(),
+    );
     println!("{:?}", verify);
 }
